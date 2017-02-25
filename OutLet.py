923f25ee5eb90b6cdf874721436268094e4df51d
Jack Brown <jackcbrown89@gmail.com>
2017-02-25 07:11:25 -0600

    new changes

--- OutLet.py
+++ OutLet.py
@@ -1,11 +1,15 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
+app.debug = 'true'
+
+dictionaryArray = [{'title': 'Test 1', 'source': 'Source 1'}, {'title': 'Test 2', 'source': 'Source 2'}]
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+
+    return render_template('index.html', searchresults=dictionaryArray)
 
 
 if __name__ == '__main__':
