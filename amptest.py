eafba1aaf41d81e8e1bfe5846f55fe2564cae68d
Jack Brown <jackcbrown89@gmail.com>
2017-02-25 19:55:52 -0600

    input for api keys

--- amptest.py
+++ amptest.py
@@ -7,7 +7,7 @@ orig_urls = ['http://www.breitbart.com/big-journalism/2015/09/26/washington-post
              'https://www.theguardian.com/membership/2016/feb/24/todays-release-of-accelerated-mobile-pages-amp',
              'http://bit.ly/28lya4p']
 
-
+api_key = input('Api key: ')
 data = dict(key=api_key, urls=orig_urls)
 
 r = requests.post(url, data=data, allow_redirects=True)
